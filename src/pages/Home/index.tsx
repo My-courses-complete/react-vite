daa622d06e5e27550ea409d79d9ad35f470f2617
Jesus Mejia <chucho4720013@gmail.com>
2023-09-06 23:50:04 -0500

    fetch data in api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import { Layout } from "../../components";
 import Card from "../../components/Card";
 
-export default function index() {
+export default function Home() {
+  const [items, setItems] = useState<Array<Product>>([]);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+    .then((response) => response.json())
+    .then((data) => setItems(data));
+  }, []);
+
   return (
     <Layout>
-      Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+      {
+        items.map((item) => (
+          <Card key={item.id} product={item}/>
+        ))
+      }
+      </div>
     </Layout>
   );
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,3 +3,22 @@
 type LayoutProps = {
 	children: React.ReactNode
 }
+
+type Product = {
+	id: number
+	title: string
+	price: number
+	description: string
+	category: Category
+	images: string[]
+}
+
+type Category = {
+	id: number
+	name: string
+	image: string
+}
+
+type CardProps = {
+	product: Product
+}
